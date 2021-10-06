c623156721cbcf3173c278fc8f8a4e574cbe0668
bie7 <bie7.habibi@gmail.com>
2021-10-06 21:35:31 +0700

    tugas

--- Level 3/Tugas 9/script.js
+++ Level 3/Tugas 9/script.js
@@ -1,5 +1,22 @@
 /*
 no 1
+Database adalah kumpulan data yang saling berhubungan yang tersimpan dalam sebuah media penyimpanan yang diatur oleh software DBMS,
+perbedaan SQL dan NoSQl adalah terletak pada bahasa perintahnya, dimana SQL perintah yang terstruktur dan berbasis relasional, sedangkan NoSQL bahasa yang dipakai adalah bahasa pemograman Javascript , berbasis JSON dan tidak terstruktur.
 
 
+no 2
+saya pernah belajar Database, yaitu Mysql . saya belajar baru sampai CRUD
+
+no3
+contoh query SQL
+1. create database info;
+2. create table bodata (id int(10), nama varchar(20), umur int(10), alamat varchar(40) );
+3. Select * form biodata;
+4. insert into biodata ( id, nama, umur , alamat) value ('Habibi', 28, 'Ciracas Lama RT 02 / RW 20');
+5. update biodata set umur = 25 where umur = 28;
+
+
+no 4
+contoh find all pada mongoDb
+  biodata.find()
 */
